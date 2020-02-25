eec96ab8336676095258ca320e8ddda0663c654e
JiviteshG <gudekar.jivitesh@gmail.com>
2020-02-25 15:39:06 -0500

    Added repeated string solution

---
+++ hacker_rank/repeated string.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 14:58:41 2020
+
+Lilah has a string, , of lowercase English letters that she repeated infinitely many times.
+
+Given an integer, , find and print the number of letter a's in the first  letters of Lilah's infinite string.
+
+For example, if the string  and , the substring we consider is , the first  characters of her infinite string. There are  occurrences of a in the substring.
+
+Function Description
+
+Complete the repeatedString function in the editor below. It should return an integer representing the number of occurrences of a in the prefix of length  in the infinitely repeating string.
+
+repeatedString has the following parameter(s):
+
+s: a string to repeat
+n: the number of characters to consider
+Input Format
+
+The first line contains a single string, .
+The second line contains an integer, .
+
+Constraints
+
+For  of the test cases, .
+Output Format
+
+Print a single integer denoting the number of letter a's in the first  letters of the infinite string created by repeating  infinitely many times.
+
+Sample Input 0
+
+aba
+10
+Sample Output 0
+
+7
+Explanation 0
+The first  letters of the infinite string are abaabaabaa. Because there are  a's, we print  on a new line.
+
+Sample Input 1
+
+a
+1000000000000
+Sample Output 1
+
+1000000000000
+Explanation 1
+Because all of the first  letters of the infinite string are a, we print  on a new line.
+
+
+@author: Jivitesh Gudekar
+"""
+
+#!/bin/python3
+
+
+# Complete the repeatedString function below.
+def repeatedString(s, n):
+    if(len(s) == 1 and s=='a'):
+        return n
+    elif(len(s) == 1):
+        return 0
+    else:
+        mul, rem = divmod(n, len(s))
+        cnt = s.count('a')
+        if(rem == 0):
+            return cnt * mul
+        else:
+            cnt_rem = s[:rem].count('a')
+            return (cnt*mul) + cnt_rem
+
+ 
+
+if __name__ == '__main__':
+
+
+    s = "kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm"
+
+    n = 736778906400
+
+    result = repeatedString(s, n)
+
+    print(str(result) + '\n')
+
